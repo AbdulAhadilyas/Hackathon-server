c836d2ebc2e3194cbf40c5a1ebd6b0f3eebb7ce2
Abdul Ahad <99830052+AbdulAhadilyas@users.noreply.github.com>
2023-02-13 15:01:17 +0500

    add url

--- server.mjs
+++ server.mjs
@@ -13,7 +13,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "*"],
+    origin: ["http://localhost:3000","https://saylani-store-9b139.web.app", "*"],
     credentials: true,
   })
 );
